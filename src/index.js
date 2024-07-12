f4232ff3c3c93454eead66d894b96edfbf129679
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2024-07-11 22:19:30 -0300

    feat: organizing middlewares

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors);
 app.use(routes);
-
-app.get('/', (request, response) => {
-  response.send('Olá mundo!');
-});
+app.use(errorHandler);
 
 app.listen(8000, () => console.log('Server started at http://localhost:8000'));
